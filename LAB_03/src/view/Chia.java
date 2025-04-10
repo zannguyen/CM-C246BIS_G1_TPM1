946c1c5f79664e26badaa239a25f8afa1bbd1768
BigHappy2005 <phucleehodai@gmail.com>
2025-04-10 07:42:19 +0700

    test1

--- LAB_03/src/view/Chia.java
+++ LAB_03/src/view/Chia.java
@@ -1,5 +1,9 @@
 package view;
 
 public class Chia {
-
+	public static String tinh(int a, int b) {
+        if (b == 0) return "Không chia được cho 0";
+        return String.valueOf(a / b);
+    }
 }
+
